749edd2a322756596ed27bc728ffcdd4fb9d2f8b
mahealexandre <mahe.alexandre@outlook.fr>
2025-01-29 00:34:41 +0100

    Update SpinGame.tsx

--- src/components/room/SpinGame.tsx
+++ src/components/room/SpinGame.tsx
@@ -69,7 +69,7 @@ export const SpinGame = ({ players, roomId }: SpinGameProps) => {
                 <Button
                   onClick={handleStopGame}
                   variant="destructive"
-                  className="w-full">
+                  className="w-full"
                 >
                   Arrêter la partie
                 </Button>
