4c8a612e45557e7914c43f67f8e3856dc2f5eab0
mahealexandre <mahe.alexandre@outlook.fr>
2025-01-29 19:46:09 +0100

    Update ActionDisplay.tsx

--- src/components/room/ActionDisplay.tsx
+++ src/components/room/ActionDisplay.tsx
@@ -6,7 +6,7 @@ interface ActionDisplayProps {
 
 export const ActionDisplay = ({ currentAction }: ActionDisplayProps) => {
   return (
-    <div className="mt-4 p-4 bg-white rounded-xl border-2 border-[#9C2FB0] w-full h-20 flex items-center justify-center overflow-hidden">
+    <div className="mt-4 p-4 bg-white rounded-xl border-2 border-[#b40064] w-full h-20 flex items-center justify-center overflow-hidden">
       <p className="text-xl text-[#000000] text-ellipsis overflow-hidden text-center break-words">
         {currentAction ? currentAction.action_text : "Aucune action en cours"}
       </p>
