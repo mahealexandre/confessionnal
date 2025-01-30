b3b1f2c8f6cbfc78ecb62169ff862122078967c7
mahealexandre <mahe.alexandre@outlook.fr>
2025-01-30 12:34:14 +0100

    Update WaitingRoom.tsx

--- src/components/room/WaitingRoom.tsx
+++ src/components/room/WaitingRoom.tsx
@@ -211,15 +211,16 @@ export const WaitingRoom = ({ code, players, onStartGame }: WaitingRoomProps) =>
             onValueChange={handleDifficultyChange}
             className="justify-center"
           >
-            <ToggleGroupItem value="sober" aria-label="Sans alcool" className="text-3xl">
+            <ToggleGroupItem value="sober" aria-label="Sans alcool" className="text-6xl">
               🙂
             </ToggleGroupItem>
-            <ToggleGroupItem value="easy" aria-label="Easy" className="text-3xl">
+            <ToggleGroupItem value="easy" aria-label="Easy" className="text-6xl">
               😳
             </ToggleGroupItem>
-            <ToggleGroupItem value="hard" aria-label="Hard" className="text-3xl">
+            <ToggleGroupItem value="hard" aria-label="Hard" className="text-6xl">
               😵‍💫
             </ToggleGroupItem>
+          </ToggleGroup>
 
           {/* Rappel des jokers */}
           <p className="text-sm text-center text-gray-600 mt-2">{jokerInfo}</p>
